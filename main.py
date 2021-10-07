31eda971f5d379b0e062fad3942ebbe46f5ee2b4
Gabe <gabepar123@gmail.com>
2021-10-07 19:05:57 -0400

    Turned Maze into class, other formatting changes

---
+++ main.py
@@ -0,0 +1,6 @@
+from maze import *
+
+
+maze = Maze()
+maze.create_maze_dfs()
+print(maze.print_maze())
--- maze.py
+++ maze.py
@@ -1,101 +1,116 @@
 import random
 
-MAZE_SIZE = 101
-maze = [["█"]*MAZE_SIZE for i in range(MAZE_SIZE)]
+class Maze:
 
+    MAZE_SIZE = 10 # Change this to change maze dimensions
+    maze = [[]]
 
+    # Default goal pos = [100][100]
+    GOAL = [MAZE_SIZE - 1, MAZE_SIZE - 1]
+    #Agent starting pos = [0][0]
+    agent_pos = [0, 0]
+    
 
+    def __init__(self):
+        self.maze = [["█"]*self.MAZE_SIZE for i in range(self.MAZE_SIZE)]
 
-def print_maze():
-    for row in maze:
-        print("[", end ="")
-        for col in row:
-            print(col, end = "")
-        print("]")
+    def print_maze(self):
+        for row in self.maze:
+            print("[", end ="")
+            for col in row:
+                print(col, end = "")
+            print("]")
 
-#
-# Creates grid world using stack DFS
-# Marking the index as "-" corresponds to UNBLOCKED
-# Marking the index as "x" corresponds to BLOCKED
-#
-def create_maze_dfs():
-    global maze
-    visited = [[False]*MAZE_SIZE for _ in range(MAZE_SIZE)]
+    #
+    # Creates grid world using stack DFS
+    # Marking the index as "-" corresponds to UNBLOCKED
+    # Marking the index as "x" corresponds to BLOCKED
+    # "A" is the Agent's position
+    # "G" is the goal index
 
-    row = start_row = random.randint(0, MAZE_SIZE - 1)
-    row = start_col = random.randint(0, MAZE_SIZE - 1)
-
-    st = []
-    st.append([start_row, start_col])
-
-    while (len(st) > 0):
-        curr = st.pop()
-        row = curr[0]
-        col = curr[1]
+    def create_maze_dfs(self):
         
-        if (row < 0 or row >= MAZE_SIZE or col < 0 or col >= MAZE_SIZE or visited[row][col]):
-            continue
-        
-        visited[row][col] = True
+        # make sure to never block the goal or initial position
+        visited = [[False]*self.MAZE_SIZE for _ in range(self.MAZE_SIZE)]
+        visited[0][0] = visited[self.MAZE_SIZE - 1][self.MAZE_SIZE - 1] = True
+        self.maze[0][0] = "A"
+        self.maze[self.MAZE_SIZE - 1][self.MAZE_SIZE - 1] = "G"
 
-        rand = random.random()
-        # we mark an index as unblocked 70% of the time
-        # or if it is the first index
-        if rand < 0.7 or (row == start_row and col == start_col) :
-            maze[row][col] = "-"
-            #we only add unblocked indexs to the stack
+
+        row = start_row = random.randint(1, self.MAZE_SIZE - 2)
+        row = start_col = random.randint(1, self.MAZE_SIZE - 2)
+
+        st = []
+        st.append([start_row, start_col])
+
+        while (len(st) > 0):
+            curr = st.pop()
+            row = curr[0]
+            col = curr[1]
             
-        else:
-            maze[row][col] = "█"
+            if (row < 0 or row >= self.MAZE_SIZE or col < 0 or col >= self.MAZE_SIZE or visited[row][col]):
+                continue
+            
+            visited[row][col] = True
 
-        st.append([row + 1, col])
-        st.append([row - 1, col])
-        st.append([row, col + 1])
-        st.append([row, col - 1])
+            rand = random.random()
+            # we mark an index as unblocked 70% of the time
+            # or if it is the first index
+            if rand < 0.7 or (row == start_row and col == start_col) :
+                self.maze[row][col] = "-"
+                #we only add unblocked indexs to the stack
+                st.append([row + 1, col])
+                st.append([row - 1, col])
+                st.append([row, col + 1])
+                st.append([row, col - 1])
+                
+            else:
+                self.maze[row][col] = "█"
+
+            
 
 
-def create_maze_backtracking():
-    global maze
-    visited = [[False]*MAZE_SIZE for _ in range(MAZE_SIZE)]
 
-    row = start_row = random.randint(0, MAZE_SIZE - 1)
-    col = start_col = random.randint(0, MAZE_SIZE - 1)
+    #TODO: delete this or finish it
+    def create_maze_backtracking(self):
+        global maze
+        visited = [[False]*MAZE_SIZE for _ in range(MAZE_SIZE)]
 
-    st = []
-    st.append([start_row, start_col])
-    visited[start_row][start_col] = True
+        row = start_row = random.randint(0, MAZE_SIZE - 1)
+        col = start_col = random.randint(0, MAZE_SIZE - 1)
 
-    while (len(st) > 0):
-        curr = st.pop()
-        row = curr[0]
-        col = curr[1]
-        
-        #if (row < 0 or row >= MAZE_SIZE or col < 0 or col >= MAZE_SIZE or visited[row][col]):
-        #    continue
-        
-        #visited[row][col] = True
+        st = []
+        st.append([start_row, start_col])
+        visited[start_row][start_col] = True
 
-        neighbors = []
+        while (len(st) > 0):
+            curr = st.pop()
+            row = curr[0]
+            col = curr[1]
+            
+            #if (row < 0 or row >= MAZE_SIZE or col < 0 or col >= MAZE_SIZE or visited[row][col]):
+            #    continue
+            
+            #visited[row][col] = True
 
-        if row + 1 < MAZE_SIZE and not visited[row + 1][col]:
-            neighbors.append([row + 1, col])
-        if row - 1 >= 0 and not visited[row - 1][col]:
-            neighbors.append([row - 1, col])
-        if col + 1 < MAZE_SIZE and not visited[row][col + 1]:
-            neighbors.append([row, col + 1])
-        if col - 1 >= 0 and not visited[row][col - 1]:
-            neighbors.append([row, col - 1])
-        
-        if len(neighbors) > 0:
-            #st.append([row, col])
-            chosen_neighbor = neighbors[random.randint(0, len(neighbors) - 1)]
-           # for n in neighbors:
-           #     if n != chosen_neighbor:
-           #         visited[n[0]][n[1]] = True
-            maze[row][col] = "-"
-            maze[chosen_neighbor[0]][chosen_neighbor[1]] = "-"
-            visited[chosen_neighbor[0]][chosen_neighbor[1]] = True
-            st.append(chosen_neighbor)
+            neighbors = []
 
-create_maze_backtracking()
-print_maze()
+            if row + 1 < MAZE_SIZE and not visited[row + 1][col]:
+                neighbors.append([row + 1, col])
+            if row - 1 >= 0 and not visited[row - 1][col]:
+                neighbors.append([row - 1, col])
+            if col + 1 < MAZE_SIZE and not visited[row][col + 1]:
+                neighbors.append([row, col + 1])
+            if col - 1 >= 0 and not visited[row][col - 1]:
+                neighbors.append([row, col - 1])
+            
+            if len(neighbors) > 0:
+                #st.append([row, col])
+                chosen_neighbor = neighbors[random.randint(0, len(neighbors) - 1)]
+            # for n in neighbors:
+            #     if n != chosen_neighbor:
+            #         visited[n[0]][n[1]] = True
+                maze[row][col] = "-"
+                maze[chosen_neighbor[0]][chosen_neighbor[1]] = "-"
+                visited[chosen_neighbor[0]][chosen_neighbor[1]] = True
+                st.append(chosen_neighbor)
